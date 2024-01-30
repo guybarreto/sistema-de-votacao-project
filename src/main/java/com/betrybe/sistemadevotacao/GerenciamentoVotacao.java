2bf78142e495e3338a94f3b0bc65f540424ea014
guybarreto <guybarreto@icloud.com>
2024-01-30 18:03:11 -0300

    Requirement 04

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,31 @@
 package com.betrybe.sistemadevotacao;
+import java.util.ArrayList;
 
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<String> pessoasCandidatas;
+  private ArrayList<String> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
 
+  public GerenciamentoVotacao() {
+  }
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
